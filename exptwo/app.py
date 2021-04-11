163d0ee5d143f550536d440247db6ebe6e9a9ad6
Harene M <77388547+Harene-M@users.noreply.github.com>
2021-04-11 19:02:22 +0530

    Add files via upload
    Program to use url_for( ) and redirect( ) in flask.

---
+++ exptwo/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/admin")
+def hello_admin():
+	return "Hello admin"
+
+@app.route("/guest/<guest>")
+def hello_guest(guest):
+	return "Hello %s" %guest
+
+@app.route("/user/<name>")
+def hello_user(name):
+	if name==admin:
+		return redirect(url_for("hello_admin")
